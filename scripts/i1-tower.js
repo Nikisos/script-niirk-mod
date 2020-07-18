f927e1a307665241d98493d8170d7568144582f3
Niirk <63856468+Niirk@users.noreply.github.com>
2020-07-18 10:05:22 -0300

    Add files via upload

---
+++ scripts/i1-tower.js
@@ -0,0 +1,7 @@
+const i1-laser = extend(BasicBulletType, {});
+i1-laser.damage = 10;
+i1-laser.bulletWidth = 6;
+i1-laser.bulletHeight = 80;
+i1-laser.bulletShrink = 0;
+i1-laser.lifetime = 1;
+i1-laser.despawnEffect = Fx.none;
