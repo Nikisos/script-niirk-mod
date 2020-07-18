608540d9b08869e3f6d75aedf9326e51b64d5c8d
Niirk <63856468+Niirk@users.noreply.github.com>
2020-07-18 12:24:07 -0300

    Update empfighter.js

--- scripts/empfighter.js
+++ scripts/empfighter.js
@@ -4,25 +4,3 @@ emp.speedMultiplier = 0;
 emp.damage = 0;
 emp.damageMultiplier = 0;
 emp.effect = none;
-//Creating a new bullet 'empMissile'
-const empMissile = extend(missileBulletType);
-empMissile.damage = 1;
-empMissile.bulletWidth = 9;
-bulletHeight = 13;
-empMissile.lifetime = 180;
-empMissile.speed = 3;
-empMissile.frontColor = a488eb;
-empMissile.backColor = 8b73c7;
-empMissile.trailColor = cb00ff;
-empMissile.homingPower = 1;
-empMissile.homingRange = 150;
-empMissile.hitEffect = blastExplosion;
-empMissile.despawnEffect = blastExplosion;
-empMissile.status = emp
-empMissile.statusDuration = 300
-//The unit
-const empfighter = extend(hoverUnit)
-empfighter.weapon = empLauncher
-//The weapon
-const empLauncher = extend(weapon)
-empLauncher.bullet = empMissile
