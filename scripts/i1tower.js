ed9dbef92029cb73e8b16499e20e7a6b596ea164
Niirk <63856468+Niirk@users.noreply.github.com>
2020-07-18 10:54:27 -0300

    Update and rename i1-tower.js to i1tower.js

---
+++ scripts/i1tower.js
@@ -0,0 +1,10 @@
+const i1Laser = extend(BasicBulletType, {});
+i1Laser.damage = 10;
+i1Laser.bulletWidth = 6;
+i1Laser.bulletHeight = 80;
+i1Laser.bulletShrink = 0;
+i1Laser.lifetime = 1;
+i1Laser.despawnEffect = Fx.none;
+const i1Tower = extend(laserTurret, i1tower{});
+i1Tower.shootType = i1-laser;
+i1Tower.update = true;
